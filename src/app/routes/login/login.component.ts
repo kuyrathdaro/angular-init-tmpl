cad419e0c55d21ef4921649bf9837977c3676149
Rathdaro Kuy <92511754+kuyrathdaro@users.noreply.github.com>
2025-04-19 03:24:39 +0000

    feat: add tailwind css and login template

--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -1,11 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core"
 
 @Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+    selector: "app-component",
+    styleUrls: ["./login.component.scss"],
+    templateUrl: './login.component.html'
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
